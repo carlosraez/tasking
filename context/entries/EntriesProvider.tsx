fbaa315d8f925d643ea43d4e5c0e86bde8bc6bc9
Carlos Raez <carlosraez@gmail.com>
2023-03-01 17:55:09 +0100

    feat entries provider

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,4 +1,5 @@
 import { FC, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { Entry } from "@/interfaces";
 import { EntriesContext, entriesReducer } from "./";
 
@@ -6,7 +7,36 @@ export interface EntriesState {
   entries: Entry[];
 }
 const Entries_INITIAL_STATE: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      status: "pending",
+      createdAt: Date.now(),
+    },
+    {
+      _id: uuidv4(),
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      status: "pending",
+      createdAt: Date.now(),
+    },
+    {
+      _id: uuidv4(),
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      status: "in-progress",
+      createdAt: Date.now() - 1000000,
+    },
+    {
+      _id: uuidv4(),
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      status: "finished",
+      createdAt: Date.now() - 100000,
+    },
+  ],
 };
 
 interface EntriesProviderProps {
